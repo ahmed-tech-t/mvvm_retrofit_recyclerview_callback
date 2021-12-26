564c7857b6a1651fdc2124801644f583e4a1a138
ahmed <codo0161606@gmail.com>
2021-12-27 00:54:17 +0300

    init Commit

---
+++ app/src/main/java/com/example/mvvm_retrofit_recyclerview_callback/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mvvm_retrofit_recyclerview_callback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
